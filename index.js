f49dfc207a037216141f48ebe3c192662c987eda
Subhajit Sahu <wolfram77@gmail.com>
2020-02-10 11:39:36 +0530

    v0.1.4

--- index.js
+++ index.js
@@ -8,11 +8,10 @@ var index = lunr(function() {
   this.field('type');
   this.field('status');
   for(var r of corpus.values())
-    this.add({key: r.code, code: r.code.replace(/[\(\)]/g, ' '), names: r.names, type: r.type, status: r.status});
+    this.add({key: r.code, code: r.code.replace(/[\(\)]/g, ' '), names: r.names, type: r.type, status: r.status.replace(/(\w)/g, 'status_$1')});
 });
 function foodins(txt) {
   txt = txt.replace(/(^|\s+)(ins\s*)?(\d\d\d+)\s*([a-z])?\s*(\([ivx]+\))?(\s+?|$)/gi, ' $3$4$5 ').replace(/[\(\)]/g, ' ');
-  // console.log(txt);
   var mats = index.search(txt), z = [];
   for(var mat of mats)
     z.push(corpus.get(mat.ref));
@@ -20,4 +19,3 @@ function foodins(txt) {
 };
 foodins.corpus = corpus;
 module.exports = foodins;
-// console.log(foodins('z ins 100 (ii)')[0]);
