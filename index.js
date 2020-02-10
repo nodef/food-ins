e959197d74e352c052bf0201e707b9e66fc24db9
Subhajit Sahu <wolfram77@gmail.com>
2020-02-10 11:38:39 +0530

    v0.0.5

---
+++ index.js
@@ -0,0 +1,21 @@
+const lunr = require('lunr');
+const corpus = require('./corpus')
+
+var index = lunr(function() {
+  this.ref('key');
+  this.field('code');
+  this.field('names');
+  this.field('type');
+  this.field('status');
+  for(var r of corpus.values())
+    this.add({key: r.code, code: r.code.replace(/[\(\)]/g, ' '), names: r.names, type: r.type, status: r.status});
+});
+function foodins(txt) {
+  txt = txt.replace(/^\s*(ins)?\s*(\d\d\d+)\s*([a-z])?\s*(\([ivx]+\))?\s*?$/i, '$2$3$4').replace(/[\(\)]/g, ' ');
+  var mats = index.search(txt), z = [];
+  for(var mat of mats)
+    z.push(corpus.get(mat.ref));
+  return z;
+};
+foodins.corpus = corpus;
+module.exports = foodins;
